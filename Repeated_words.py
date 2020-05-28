af832d0bf688244f8b3c2e24cc67ae7a35537648
Digant Kumar <63453594+digantkumar@users.noreply.github.com>
2020-05-28 12:09:54 +0100

    Add files via upload
    Program which detects repeated words in a text.

---
+++ Repeated_words.py
@@ -0,0 +1,17 @@
+def find_repeats(fname):
+    textfile = open(fname,"r")
+    content = textfile.read().lower()
+    new_word = ""
+    for word in content:
+        if word == "." or word == "?" or word == ",":
+            continue
+        else:
+            new_word = new_word + word
+    word_split = new_word.split()
+    for word in range(len(word_split)-1):
+        if word_split[word] == word_split[word + 1]:
+            print("Repeated word found: ", word_split[word])
+    return ""
+
+file = "C://Users//Digant//Desktop//Digant Study docs//Interview etc//Behavioral Questions.txt"
+print(find_repeats(file))
